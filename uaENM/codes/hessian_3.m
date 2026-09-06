2fa51c9dd9b59c93fd09561469168f685df9e73c
ChunhuaLab <139953687+ChunhuaLab@users.noreply.github.com>
2026-09-06 09:50:38 +0800

    Update hessian_3.m

--- uaENM/codes/hessian_3.m
+++ uaENM/codes/hessian_3.m
@@ -1,6 +1,5 @@
 function [hessian3]=hessian_3(angleparam,angle,xyz_cg)
 
-
 % referenced calculation principle: Blondel and Karplus, J. Comput. Chem., Vol. 17, No. 9, 1132-1141 (1996)
 % reference code: Na, H.; Song, G., Phys. Biol. 2023, 20, 46005.
 
